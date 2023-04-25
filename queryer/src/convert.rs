850151281790ad1d3674a1ed6ffecfa490ada55a
cracker <2278801557@qq.com>
2023-04-25 17:32:39 +0800

    queryer:coding

---
+++ queryer/src/convert.rs
@@ -0,0 +1,8 @@
+pub struct Sql<'a> {
+    pub(crate) selection: Vec<Expr>,
+    pub(crate) condition: Option<Expr>,
+    pub(crate) source: &'a str,
+    pub(crate) order_by: Vec<(String, bool)>,
+    pub(crate) offset: Option<i64>,
+    pub(crate) limit: Option<usize>,
+}
